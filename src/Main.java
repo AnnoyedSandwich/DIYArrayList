2187ddaa994df4014762bf42691498f9e260612b
AnnoyedSandwich <41872159+AnnoyedSandwich@users.noreply.github.com>
2023-07-09 22:21:44 +0200

    added get test

--- src/Main.java
+++ src/Main.java
@@ -12,14 +12,15 @@ public class Main {
         test.add(9);
         test.add(10);
         test.add(11);
-        System.out.println("contains " + test.contains(1));
-        System.out.println("size " + test.size());
-        System.out.println("is empty " + test.isEmpty());
-        System.out.println("print " + test.print());
+        System.out.println("get at 5: " + test.get(5));
+        System.out.println("contains 1: " + test.contains(1));
+        System.out.println("size: " + test.size());
+        System.out.println("is empty: " + test.isEmpty());
+        System.out.println("print all: " + test.print());
         test.delete(2);
-        System.out.println("delete test " + test.print());
+        System.out.println("delete 2: " + test.print());
         test.insert(1,5);
-        System.out.println("insert test " + test.print());
+        System.out.println("insert 5@1: " + test.print());
 
     }
 }
